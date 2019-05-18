1e15f40a5cc444037f3f14e388384fde55fd950f
gganebnyi <gganebnyi@fusionworks.md>
2019-05-18 17:15:05 +0300

    chngaed port

--- admin-ui/src/App.js
+++ admin-ui/src/App.js
@@ -3,7 +3,7 @@ import { Admin, Resource, ShowGuesser, ListGuesser } from 'react-admin';
 import crudProvider from '@fusionworks/ra-data-nest-crud';
 import { GuestCreate, GuestEdit } from './Guests';
 
-const dataProvider = crudProvider('http://localhost:3000');
+const dataProvider = crudProvider('http://localhost:3001');
 const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="guests" list={ListGuesser} create={GuestCreate} edit={GuestEdit} show={ShowGuesser} />
