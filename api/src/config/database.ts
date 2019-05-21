56123ff6e5e9135ae6ae4325af27735ecb7a9374
gganebnyi <gganebnyi@fusionworks.md>
2019-05-21 16:22:03 +0300

    updated config options

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -5,7 +5,7 @@ export default {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  entities: [process.env.DB_ENTITIES],
+  entities: ['src/**/*.entity{.ts,.js}'],
   synchronize: process.env.DB_SYNCRONIZE === 'true',
   logging: process.env.DB_LOGGING === 'true',
 };
